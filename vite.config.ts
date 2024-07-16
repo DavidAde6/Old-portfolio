b840168c870bc4468da08608aa0e2e986a1091e6
David Adeniyi <dadeniyi54@gmail.com>
2024-07-16 17:26:41 -0500

    deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/davidAde6.github.io/"
+  base: "/davidade6.github.io/"
 })
