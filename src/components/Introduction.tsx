5264a8ad3b153033c76da87a128cd2d610e1527d
David Adeniyi <dta123adeniyi@gmail.com>
2024-07-31 21:48:47 -0500

    testing features

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -5,7 +5,7 @@ function Introduction() {
     return (
         <>
         <p className='test'> David Adeniyi</p>
-        <p className='test'> Carleton CS Student</p>
+        <p className='test'> Carleton CS Student TESTING</p>
         </>
     )
 }
