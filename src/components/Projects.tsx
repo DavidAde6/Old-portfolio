561921f271355459450fbe995b83f8eeb6bffe0e
David Adeniyi <dta123adeniyi@gmail.com>
2024-08-24 09:32:20 -0500

    optimized projects again

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,41 +1,65 @@
 function Projects() {
   // OPTIMIZE CODE WHEN U HAVE TIME!!!
   window.addEventListener("scroll", () => {
+    // BRO USE THE PROJECT IMAGE TO CHECK INSTEAD
+    const projects = document.getElementsByClassName("relative");
     const project1 = document.getElementById("id-1")!;
-    const rect1 = project1.getBoundingClientRect();
+    const images = document.getElementsByClassName("projectImage");
+    const rect1 = images[0].getBoundingClientRect();
     const project2 = document.getElementById("id-2")!;
-    const rect2 = project2.getBoundingClientRect();
+    const rect2 = images[1].getBoundingClientRect();
     const project3 = document.getElementById("id-3")!;
-    const rect3 = project3.getBoundingClientRect();
+    const rect3 = images[2].getBoundingClientRect();
     const project4 = document.getElementById("id-4")!;
-    const rect4 = project4.getBoundingClientRect();
+    const rect4 = images[3].getBoundingClientRect();
     const project5 = document.getElementById("id-5")!;
-    const rect5 = project5.getBoundingClientRect();
+    const rect5 = images[4].getBoundingClientRect();
     const project6 = document.getElementById("id-6")!;
-    const rect6 = project6.getBoundingClientRect();
+    const rect6 = images[5].getBoundingClientRect();
+    console.log(rect6.top, rect6.bottom);
     if (rect6.top <= 0 && rect6.bottom >= 0) {
       project5.classList.remove("fixed");
+      project5.classList.add("relative");
       project6.classList.remove("relative");
       project6.classList.add("fixed");
     } else if (rect5.top <= 0 && rect5.bottom >= 0) {
       project4.classList.remove("fixed");
+      project4.classList.add("relative");
       project5.classList.remove("relative");
       project5.classList.add("fixed");
+      project6.classList.remove("fixed");
+      project6.classList.add("relative");
     } else if (rect4.top <= 0 && rect4.bottom >= 0) {
       project3.classList.remove("fixed");
+      project3.classList.add("relative");
       project4.classList.remove("relative");
       project4.classList.add("fixed");
+      project5.classList.remove("fixed");
+      project5.classList.add("relative");
     } else if (rect3.top <= 0 && rect3.bottom >= 0) {
       project2.classList.remove("fixed");
+      project2.classList.add("relative");
       project3.classList.remove("relative");
       project3.classList.add("fixed");
+      project4.classList.remove("fixed");
+      project4.classList.add("relative");
     } else if (rect2.top <= 0 && rect2.bottom >= 0) {
       project1.classList.remove("fixed");
+      project1.classList.add("relative");
       project2.classList.remove("relative");
       project2.classList.add("fixed");
+      project3.classList.remove("fixed");
+      project3.classList.add("relative");
     } else if (rect1.top <= 0 && rect1.bottom >= 0) {
       project1.classList.remove("relative");
       project1.classList.add("fixed");
+      project2.classList.remove("fixed");
+      project2.classList.add("relative");
+    } else {
+      project1.classList.remove("fixed");
+      project1.classList.add("relative");
+      project6.classList.remove("fixed");
+      project6.classList.add("relative");
     }
   });
   return (
@@ -140,178 +164,168 @@ function Projects() {
       </div>
 
       <div className="projectSlide">
-        <div className="projectContents">
-          <div className="relative" id="id-3">
-            <h1 className="projectHeader">03</h1>
-            <div className="projectBody">
-              <h1 className="projectTitle">Titanic Survival Estimator</h1>
-              <img
-                className="display-img"
-                src="https://raw.githubusercontent.com/DavidAde6/DavidAde6.github.io/main/src/assets/images/PyTorch.png"
-                alt="PyTorch"
-              />
-              <img
-                className="display-img"
-                src="https://raw.githubusercontent.com/DavidAde6/DavidAde6.github.io/main/src/assets/images/numpy-logo.png"
-                alt="Numpy"
-              />
-              <img
-                className="display-img"
-                src="https://raw.githubusercontent.com/DavidAde6/DavidAde6.github.io/main/src/assets/images/pandas-logo.png"
-                alt="Pandas"
-              />
-              <img
-                className="display-img"
-                src="https://raw.githubusercontent.com/DavidAde6/DavidAde6.github.io/main/src/assets/images/Typescript_logo.png"
-                alt="TypeScript"
-              />
-              <img
-                className="display-img"
-                src="https://raw.githubusercontent.com/DavidAde6/DavidAde6.github.io/main/src/assets/images/css-logo.png"
-                alt="CSS"
-              />
-              <img
-                className="display-img"
-                src="https://raw.githubusercontent.com/DavidAde6/DavidAde6.github.io/main/src/assets/images/html-logo.png"
-                alt="HTML"
-              />
-              <div className="projectParagraph">
-                <p>
-                  Powered by pytorch, and frameworks like pandas, and numpy to
-                  determine whether an individual might have survived on the
-                  titanic. Built with by Python, Typescript, HTML & CSS
-                </p>
-              </div>
+        <div className="relative" id="id-3">
+          <h1 className="projectHeader">03</h1>
+          <div className="projectBody">
+            <h1 className="projectTitle">Titanic Survival Estimator</h1>
+            <img
+              className="display-img"
+              src="https://raw.githubusercontent.com/DavidAde6/DavidAde6.github.io/main/src/assets/images/PyTorch.png"
+              alt="PyTorch"
+            />
+            <img
+              className="display-img"
+              src="https://raw.githubusercontent.com/DavidAde6/DavidAde6.github.io/main/src/assets/images/numpy-logo.png"
+              alt="Numpy"
+            />
+            <img
+              className="display-img"
+              src="https://raw.githubusercontent.com/DavidAde6/DavidAde6.github.io/main/src/assets/images/pandas-logo.png"
+              alt="Pandas"
+            />
+            <img
+              className="display-img"
+              src="https://raw.githubusercontent.com/DavidAde6/DavidAde6.github.io/main/src/assets/images/Typescript_logo.png"
+              alt="TypeScript"
+            />
+            <img
+              className="display-img"
+              src="https://raw.githubusercontent.com/DavidAde6/DavidAde6.github.io/main/src/assets/images/css-logo.png"
+              alt="CSS"
+            />
+            <img
+              className="display-img"
+              src="https://raw.githubusercontent.com/DavidAde6/DavidAde6.github.io/main/src/assets/images/html-logo.png"
+              alt="HTML"
+            />
+            <div className="projectParagraph">
+              <p>
+                Powered by pytorch, and frameworks like pandas, and numpy to
+                determine whether an individual might have survived on the
+                titanic. Built with by Python, Typescript, HTML & CSS
+              </p>
             </div>
-            <a href="" className="projectLink">
-              <button className="projectLinkButton">UNAVAILABLE</button>
-            </a>
           </div>
+          <a href="" className="projectLink">
+            <button className="projectLinkButton">UNAVAILABLE</button>
+          </a>
         </div>
         <div className="projectImage"></div>
       </div>
 
       <div className="projectSlide">
-        <div className="projectContents">
-          <div className="relative" id="id-4">
-            <h1 className="projectHeader">04</h1>
-            <div className="projectBody">
-              <h1 className="projectTitle">Lakes & River Path Optimisation</h1>
-              <img
-                className="display-img"
-                src="https://raw.githubusercontent.com/DavidAde6/DavidAde6.github.io/main/src/assets/images/PyTorch.png"
-                alt="PyTorch"
-              />
-              <img
-                className="display-img"
-                src="https://raw.githubusercontent.com/DavidAde6/DavidAde6.github.io/main/src/assets/images/numpy-logo.png"
-                alt="Numpy"
-              />
-              <img
-                className="display-img"
-                src="https://raw.githubusercontent.com/DavidAde6/DavidAde6.github.io/main/src/assets/images/pandas-logo.png"
-                alt="Pandas"
-              />
-              <div className="projectParagraph">
-                <p>MORE INFO SOON</p>
-              </div>
+        <div className="relative" id="id-4">
+          <h1 className="projectHeader">04</h1>
+          <div className="projectBody">
+            <h1 className="projectTitle">Lakes & River Path Optimisation</h1>
+            <img
+              className="display-img"
+              src="https://raw.githubusercontent.com/DavidAde6/DavidAde6.github.io/main/src/assets/images/PyTorch.png"
+              alt="PyTorch"
+            />
+            <img
+              className="display-img"
+              src="https://raw.githubusercontent.com/DavidAde6/DavidAde6.github.io/main/src/assets/images/numpy-logo.png"
+              alt="Numpy"
+            />
+            <img
+              className="display-img"
+              src="https://raw.githubusercontent.com/DavidAde6/DavidAde6.github.io/main/src/assets/images/pandas-logo.png"
+              alt="Pandas"
+            />
+            <div className="projectParagraph">
+              <p>MORE INFO SOON</p>
             </div>
-            <a href="https://davidade6.github.io/" className="projectLink">
-              <button className="projectLinkButton">UNAVAILABLE</button>
-            </a>
           </div>
+          <a href="https://davidade6.github.io/" className="projectLink">
+            <button className="projectLinkButton">UNAVAILABLE</button>
+          </a>
         </div>
         <div className="projectImage"></div>
       </div>
 
       <div className="projectSlide">
-        <div className="projectContents">
-          <div className="relative" id="id-5">
-            <h1 className="projectHeader">05</h1>
-            <div className="projectBody">
-              <h1 className="projectTitle">Game Development (Roblox)</h1>
-              <img
-                className="display-img"
-                src="https://raw.githubusercontent.com/DavidAde6/DavidAde6.github.io/main/src/assets/images/Lua-Logo.png"
-                alt="Lua"
-              />
-              <div className="projectParagraph">
-                <p>
-                  Aided with the scripting of a roblox game using LUA.
-                  Collaborated with a team of developers to build a game on the
-                  roblox platformss.
-                </p>
-              </div>
+        <div className="relative" id="id-5">
+          <h1 className="projectHeader">05</h1>
+          <div className="projectBody">
+            <h1 className="projectTitle">Game Development (Roblox)</h1>
+            <img
+              className="display-img"
+              src="https://raw.githubusercontent.com/DavidAde6/DavidAde6.github.io/main/src/assets/images/Lua-Logo.png"
+              alt="Lua"
+            />
+            <div className="projectParagraph">
+              <p>
+                Aided with the scripting of a roblox game using LUA.
+                Collaborated with a team of developers to build a game on the
+                roblox platformss.
+              </p>
             </div>
-            <a
-              href="https://www.roblox.com/games/16757765700/02"
-              className="projectLink"
-            >
-              <button className="projectLinkButton">Link</button>
-            </a>
           </div>
+          <a
+            href="https://www.roblox.com/games/16757765700/02"
+            className="projectLink"
+          >
+            <button className="projectLinkButton">Link</button>
+          </a>
         </div>
         <div className="projectImage"></div>
       </div>
 
       <div className="projectSlide">
-        <div className="projectContents">
-          <div className="relative" id="id-6">
-            <h1 className="projectHeader">06</h1>
-            <div className="projectBody">
-              <h1 className="projectTitle">AI Video Analysis</h1>
-              <img
-                className="display-img"
-                src="https://raw.githubusercontent.com/DavidAde6/DavidAde6.github.io/main/src/assets/images/PyTorch.png"
-                alt="PyTorch"
-              />
-              <img
-                className="display-img"
-                src="https://raw.githubusercontent.com/DavidAde6/DavidAde6.github.io/main/src/assets/images/numpy-logo.png"
-                alt="Numpy"
-              />
-              <img
-                className="display-img"
-                src="https://raw.githubusercontent.com/DavidAde6/DavidAde6.github.io/main/src/assets/images/pandas-logo.png"
-                alt="Pandas"
-              />
-              <img
-                className="display-img"
-                src="https://raw.githubusercontent.com/DavidAde6/DavidAde6.github.io/main/src/assets/images/React.png"
-                alt="React"
-              />
-              <img
-                className="display-img"
-                src="https://raw.githubusercontent.com/DavidAde6/DavidAde6.github.io/main/src/assets/images/Typescript_logo.png"
-                alt="TypeScript"
-              />
-              <img
-                className="display-img"
-                src="https://raw.githubusercontent.com/DavidAde6/DavidAde6.github.io/main/src/assets/images/css-logo.png"
-                alt="CSS"
-              />
-              <img
-                className="display-img"
-                src="https://raw.githubusercontent.com/DavidAde6/DavidAde6.github.io/main/src/assets/images/html-logo.png"
-                alt="HTML"
-              />
-              <div className="projectParagraph">
-                <p>MORE INFO SOON</p>
-              </div>
+        <div className="relative" id="id-6">
+          <h1 className="projectHeader">06</h1>
+          <div className="projectBody">
+            <h1 className="projectTitle">AI Video Analysis</h1>
+            <img
+              className="display-img"
+              src="https://raw.githubusercontent.com/DavidAde6/DavidAde6.github.io/main/src/assets/images/PyTorch.png"
+              alt="PyTorch"
+            />
+            <img
+              className="display-img"
+              src="https://raw.githubusercontent.com/DavidAde6/DavidAde6.github.io/main/src/assets/images/numpy-logo.png"
+              alt="Numpy"
+            />
+            <img
+              className="display-img"
+              src="https://raw.githubusercontent.com/DavidAde6/DavidAde6.github.io/main/src/assets/images/pandas-logo.png"
+              alt="Pandas"
+            />
+            <img
+              className="display-img"
+              src="https://raw.githubusercontent.com/DavidAde6/DavidAde6.github.io/main/src/assets/images/React.png"
+              alt="React"
+            />
+            <img
+              className="display-img"
+              src="https://raw.githubusercontent.com/DavidAde6/DavidAde6.github.io/main/src/assets/images/Typescript_logo.png"
+              alt="TypeScript"
+            />
+            <img
+              className="display-img"
+              src="https://raw.githubusercontent.com/DavidAde6/DavidAde6.github.io/main/src/assets/images/css-logo.png"
+              alt="CSS"
+            />
+            <img
+              className="display-img"
+              src="https://raw.githubusercontent.com/DavidAde6/DavidAde6.github.io/main/src/assets/images/html-logo.png"
+              alt="HTML"
+            />
+            <div className="projectParagraph">
+              <p>MORE INFO SOON</p>
             </div>
-            <a href="" className="projectLink">
-              <button className="projectLinkButton">UNAVAILABLE</button>
-            </a>
           </div>
+          <a href="" className="projectLink">
+            <button className="projectLinkButton">UNAVAILABLE</button>
+          </a>
         </div>
         <div className="projectImage"></div>
       </div>
 
       <div className="projectSlide">
-        <div className="projectContents">
-          <div className="relative">
-            <div className="projectBody"></div>
-          </div>
+        <div className="relative">
+          <div className="projectBody"></div>
         </div>
         <div className="projectImage"></div>
       </div>
